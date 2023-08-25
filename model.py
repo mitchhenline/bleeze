e9f107b43aa5475499e86945c0d50d24f1100c87
Mitch Henline <rmhenline@gmail.com>
2023-08-24 21:51:24 -0600

    app is running

--- model.py
+++ model.py
@@ -1,15 +1,23 @@
-"""Database models for Bleeze"""
+"""Database model for Bleeze"""
 
 import os
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# STORE
+# Each store has multiple units
+# Each store has multiple retail items
 
+# UNITS
+# many units per store
+# multiple units per contace
 
+# RENTERS
+#One renter can have multiple units
 
-
-
+# RETAIL ITEMS
+# multiple retail items per store
 
 def connect_to_db(flask_app, db_uri=os.environ["POSTGRES_URI"], echo=False):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
---
+++ server.py
@@ -0,0 +1,19 @@
+"""Bleeze Server"""
+
+from flask import Flask, render_template
+from jinja2 import StrictUndefined
+from model import connect_to_db
+
+app = Flask(__name__)
+app.secret_key = "selfstorage"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def store_select():
+    """View store selector."""
+    return render_template("store_selector.html")
+    
+
+if __name__ == "__main__":
+    # connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
