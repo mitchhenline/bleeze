e95265aede8fe782912cd7c82da0104594f09ffa
Mitch Henline <rmhenline@gmail.com>
2023-08-28 21:31:50 -0600

    more modeling done

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class Store(db.Model):
 
     __tablename__= "stores"
 
-    id = db.Column(db.Integer, autoincrement=True, primarykey=True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     store_number = db.Column(db.Integer, unique = True)
     address = db.Column(db.String(255))
     city = db.Column(db.String(255))
@@ -18,33 +18,45 @@ class Store(db.Model):
     zip = db.Column(db.Integer)
 
     units = db.relationship("Unit", backref = "store")
-    retail_items = db.relationship("Retail Items", backref = "store")
+    retail_items = db.relationship("Retail_Items", backref = "store")
 
     def __repr__(self):
         return f'Store number: {self.store_number} -- {self.city}, {self.state}'
+    
+class Renter(db.Model):
+    """A renter."""
+
+    __tablename__ = "renters"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    # ADD MORE HERE
+
+    units = db.relationship("Unit", backref = "renter")
+
+    def __repr__(self):
+        return f'Renter: {self.first_name} {self.last_name}'
     
 class Unit(db.Model):
     """A unit."""
 
     __tablename__= "units"
 
-    id = db.Column(db.Integer, autoincrement=True, primarykey=True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     unit_number = db.Column(db.String(255), unique = True)
-    size = db.Column(db.String)
+    size = db.Column(db.String(50))
     rented = db.Column(db.Boolean)
     digital_access = db.Column(db.Boolean)
-    type = db.Column(db.String)
+    type = db.Column(db.String(50))
 
+    renter_id = db.Column(db.Integer, db.ForeignKey('renters.id'))
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
-    
+
 
     def __repr__(self):
         return f'Unit number: {self.unit_number}'
 
-
-
-# RENTERS
-#One renter can have multiple units
 
 # RETAIL ITEMS
 # multiple retail items per store
