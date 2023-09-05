58168f5e994b8879992def2c1aca5920c7ddcc27
Mitch Henline <rmhenline@gmail.com>
2023-09-05 10:30:27 -0600

    form adjustment

--- forms.py
+++ forms.py
@@ -2,4 +2,4 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, validators
 
 class StoreIDForm(FlaskForm):
-    store_id = IntegerField('Store ID ', [validators.InputRequired()])
+    store_id = IntegerField('Store ID', [validators.InputRequired()], render_kw={'placeholder': 'Store ID'})
